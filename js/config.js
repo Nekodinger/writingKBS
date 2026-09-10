0b22d1e7a5c45ac6413b31b5dba2aa3c5b1995c0
Nekodinger <187770756+Nekodinger@users.noreply.github.com>
2026-09-10 13:13:55 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-const API_URL="PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+const API_URL="https://script.google.com/macros/s/AKfycbyZkUbjffWme7oYH50JsyBIamSmzs5Jr22y1zyfExzzMA1CDiyqf880h8RFKb_5uMBw/exec";
